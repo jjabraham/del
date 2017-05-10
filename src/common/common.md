2ac05c5c2bc3f3115eac91fb41808d7a772fe667
John J Abraham <john.abraham@yahoo.com>
2017-05-10 15:16:33 +1000

    added markdown files

---
+++ src/common/common.md
@@ -0,0 +1,5 @@
+### Common components folder-structure
+
+* This folder contains common components that can be reused anywhere in the rest of the site.
+* These components have their own style which is based on the assets provided by the designer.
+* One way to improve these components would be to have the styles passed in as props so that the styling of the components can be changed depending on where they are used. This will increase the reusability of the components.
--- src/landing-page/components/ImageBlock.js
+++ src/landing-page/components/ImageBlock.js
@@ -2,6 +2,20 @@ import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 import './ImageBlock.css'
 
+/*
+There is huge scope for improving this component.
+There are a few variations of the image block in the
+design.
+- Plain image
+- A row of two images
+- Image with blurb
+- Image with caption
+- A row of two images with captions
+These variations can be handled my a more intelligent
+component that can receive values in its props to render
+the different types of images found in the design.
+
+*/
 const ImageBlock = ({ images }) => {
   const prefix = 'ImageBlock_';
   const items = images.map((image) => (
